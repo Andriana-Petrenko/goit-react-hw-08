25970353ef2de3dd361101a18f8e609e44eb37c7
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-04-26 00:40:09 +0300

    add Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import Layout from './Layout/Layout';
 import { Route, Routes } from 'react-router-dom';
 import { refreshUser } from '../redux/auth/operations';
 import NoFoundPage from '../pages/NoFoundPage/NoFoundPage';
+import RestrictedRoute from './RestrictedRoute/RestrictedRoute';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 
 
 
@@ -28,9 +30,14 @@ function App() {
       <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/register" element={
+            <RestrictedRoute>
+              <RegisterPage />
+            </RestrictedRoute>} />
+        <Route path="/login" element={<RestrictedRoute>
+              <LoginPage />
+            </RestrictedRoute>} />
+          <Route path="/contacts" element={<PrivateRoute><ContactsPage /></PrivateRoute>} />
           <Route path="*" element={<NoFoundPage/>}/>
         </Routes>
     </Layout>
